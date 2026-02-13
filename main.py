1f5d5424fd739c76ff4302d50a5ce82b495ab798
Carlos Villa <carlosvillablanco@gmail.com>
2026-02-13 21:47:36 +0100

    Migrate to uv package manager

--- main.py
+++ main.py
@@ -1,65 +1,83 @@
-import time
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
 import json
-from web_scrappers.scraper import Scraper, get_next_weekends
 import logging.config
+import time
+
+from selenium.webdriver.common.by import By
+
+import undetected_chromedriver as uc
+from web_scrappers.scraper import Scraper, get_next_weekends
 
 logger = logging.getLogger("root")
 
-def main():
 
+def main():
     # Configure logging
-    logging.config.fileConfig('logging.conf', disable_existing_loggers=True, encoding=None)
-    
+    logging.config.fileConfig(
+        "logging.conf", disable_existing_loggers=True, encoding=None
+    )
+
     # Retrieve user settings
     settings = load_settings()
     num_weeks_to_analyse = settings["num_weeks_to_analyse"]
     origins = settings["origins"]
     destinations = settings["destinations"]
-    
+
     # Get proxies
     proxies = []
-    #proxies = get_free_proxies()
-    logger.debug("Number of proxies found:", len(proxies))
-    
+    # proxies = get_free_proxies()
+    logger.debug("Number of proxies found: %s", len(proxies))
+
     # Create scraper instance
     scrapper = Scraper(settings, proxies)
 
     # Get dates next weekends
     weekends = get_next_weekends(num_weeks_to_analyse)
-    while(True):
+    while True:
         for weekend in weekends:
             for from_city in origins:
                 for idx, to_city in enumerate(destinations):
-                    logger.info("Checking flights from " + from_city + " to " + to_city + " [" + weekend[0] + " to "
-                        + weekend[1] + "] - Destination " + str(idx + 1) + "/" + str(len(destinations)))
+                    logger.info(
+                        "Checking flights from "
+                        + from_city
+                        + " to "
+                        + to_city
+                        + " ["
+                        + weekend[0]
+                        + " to "
+                        + weekend[1]
+                        + "] - Destination "
+                        + str(idx + 1)
+                        + "/"
+                        + str(len(destinations))
+                    )
                     scrapper.scrape_flights(from_city, to_city, weekend)
         time.sleep(7200)
 
+
 # Function to load user settings
-def load_settings(filepath='settings.json'):
+def load_settings(filepath="settings.json"):
     try:
-        with open(filepath, 'r') as file:
+        with open(filepath, "r") as file:
             settings = json.load(file)
     except FileNotFoundError:
         settings = {}  # Return an empty dict if the file does not exist
     return settings
 
+
 def get_free_proxies():
     logger.trace("Retrieving proxies...")
     driver = uc.Chrome()
-    driver.get('https://sslproxies.org')
-    table = driver.find_element(By.TAG_NAME, 'table')
-    thead = table.find_element(By.TAG_NAME, 'thead').find_elements(By.TAG_NAME, 'th')
-    tbody = table.find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')
+    driver.get("https://sslproxies.org")
+    table = driver.find_element(By.TAG_NAME, "table")
+    thead = table.find_element(By.TAG_NAME, "thead").find_elements(By.TAG_NAME, "th")
+    tbody = table.find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
     headers = []
     for th in thead:
         headers.append(th.text.strip())
     proxies = []
     for tr in tbody:
         proxy_data = {}
-        tds = tr.find_elements(By.TAG_NAME, 'td')
+        tds = tr.find_elements(By.TAG_NAME, "td")
         for i in range(len(headers)):
             proxy_data[headers[i]] = tds[i].text.strip()
         proxies.append(proxy_data)
@@ -67,5 +85,6 @@ def get_free_proxies():
     print("Proxies retrieved!")
     return proxies
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- web_scrappers/scraper.py
+++ web_scrappers/scraper.py
@@ -1,68 +1,81 @@
-import traceback
 import datetime
 import importlib
+import logging
+import traceback
+
 from emailsender.EmailSender import EmailSender
 from flights.InterestingFlightCache import InterestingFlightCache
 from web_scrappers.RyanairWebScrapper import RyanairWebScrapper
-from web_scrappers.IberiaWebScrapper import IberiaWebScrapper
-from web_scrappers.IberiaExpressWebScrapper import IberiaExpressWebScrapper
-from web_scrappers.SkyscannerWebScrapper import SkyscannerWebScrapper
 
-import logging
 logger = logging.getLogger("root")
 
-class Scraper:
 
+class Scraper:
     def __init__(self, settings, proxies):
         self.email_sender = EmailSender()
-        self.interesting_flight_cache = InterestingFlightCache(settings["filter_repeated_destinations_but_more_expensive"])
-        self.web_scrappers = [] 
+        self.interesting_flight_cache = InterestingFlightCache(
+            settings["filter_repeated_destinations_but_more_expensive"]
+        )
+        self.web_scrappers = []
 
         for websites_scrapper in settings["websites_scrappers"]:
-            ScrapperClass = getattr(importlib.import_module(__name__), websites_scrapper)
+            ScrapperClass = getattr(
+                importlib.import_module(__name__), websites_scrapper
+            )
             web_scrapper = ScrapperClass(proxies, **settings)
             self.web_scrappers.append(web_scrapper)
-
 
     def scrape_flights(self, from_city, to_city, weekend):
         try:
             for web_scrapper in self.web_scrappers:
                 # Retrieve information about the desired flight and weekend
-                is_flight_interesting, round_flight = web_scrapper.scrape(from_city, to_city, weekend[0], weekend[1])
+                is_flight_interesting, round_flight = web_scrapper.scrape(
+                    from_city, to_city, weekend[0], weekend[1]
+                )
 
                 if is_flight_interesting:
                     logger.info("An interesting flight was found!")
                     logger.info(round_flight)
 
                     # Check if a cheaper flight was already reported
-                    is_cheaper_than_before = self.interesting_flight_cache.save_flight(round_flight)
+                    is_cheaper_than_before = self.interesting_flight_cache.save_flight(
+                        round_flight
+                    )
                     if not is_cheaper_than_before:
-                        logger.info("The flight is not cheaper than a previously found option. Skipping notification")
+                        logger.info(
+                            "The flight is not cheaper than a previously found option. Skipping notification"
+                        )
                     else:
                         self.email_sender.send_flight(round_flight)
                 else:
                     if round_flight is not None:
-                        logger.info(f"No interesting flights found. Cheapest: {round_flight.get_total_price():.2f}€")
+                        logger.info(
+                            f"No interesting flights found. Cheapest: {round_flight.get_total_price():.2f}€"
+                        )
                     else:
                         logger.info("No interesting flights found")
         except Exception:
             logger.error(traceback.format_exc())
-        #web_scrapper.close_scrapper()
+        # web_scrapper.close_scrapper()
+
 
 def get_next_friday():
     today = datetime.date.today()
-    friday = today + datetime.timedelta((3-today.weekday())%7+1)
+    friday = today + datetime.timedelta((3 - today.weekday()) % 7 + 1)
     return friday
+
 
 def get_next_saturday():
     today = datetime.date.today()
-    friday = today + datetime.timedelta((4-today.weekday())%7+1)
+    friday = today + datetime.timedelta((4 - today.weekday()) % 7 + 1)
     return friday
+
 
 def get_next_sunday():
     today = datetime.date.today()
-    sunday = today + datetime.timedelta((5-today.weekday())%7+1)
+    sunday = today + datetime.timedelta((5 - today.weekday()) % 7 + 1)
     return sunday
+
 
 def get_next_weekends(num_weeks_to_analyse):
     weekends = []
@@ -72,9 +85,11 @@ def get_next_weekends(num_weeks_to_analyse):
         today = today + datetime.timedelta(3)
     first_day = get_next_friday()
     sunday = today + datetime.timedelta((5 - today.weekday()) % 7 + 1)
-    weekends.append((first_day.strftime('%d/%m/%Y'), sunday.strftime('%d/%m/%Y')))
+    weekends.append((first_day.strftime("%d/%m/%Y"), sunday.strftime("%d/%m/%Y")))
     for idx_week in range(1, num_weeks_to_analyse):
         next_first_day = first_day + datetime.timedelta(7 * idx_week)
         next_sunday = sunday + datetime.timedelta(7 * idx_week)
-        weekends.append((next_first_day.strftime('%d/%m/%Y'), next_sunday.strftime('%d/%m/%Y')))
+        weekends.append(
+            (next_first_day.strftime("%d/%m/%Y"), next_sunday.strftime("%d/%m/%Y"))
+        )
     return weekends
